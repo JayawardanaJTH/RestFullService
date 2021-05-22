7dd6567ebd71478d91dde56f2eae1dabf5329966
Jayawardana JTH <49198952+JayawardanaJTH@users.noreply.github.com>
2021-05-23 01:03:57 +0530

    Add libraries

--- StoreServices/src/store/services/UserService.java
+++ StoreServices/src/store/services/UserService.java
@@ -1,5 +1,49 @@
 package store.services;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import Decoder.BASE64Decoder;
+import store.model.User;
+import store.utill.DatabaseImpl;
+import store.utill.IDatabase;
+
+@Path("/UserService")
 public class UserService {
+
+	@Context
+	private HttpServletRequest request;
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public User getUser() {
+		String decoded = null;
+		User user = null;
+		IDatabase db = new DatabaseImpl();
+
+		try {
+			String header = request.getHeader("authorization");
+			System.out.println(header);
+			String data = header.substring(header.indexOf(" ") + 1);
+
+			byte[] bytes = new BASE64Decoder().decodeBuffer(data);
+			decoded = new String(bytes);
+		} catch (Exception ex) {
+			user = new User();
+
+			return user;
+		}
+
+		String userData[] = decoded.split(":");
+		System.out.println(userData);
+
+		user = db.getUser(userData[0], userData[1]);
+
+		return user;
+	}
 
 }
