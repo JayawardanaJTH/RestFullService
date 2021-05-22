8c6fed12ee9de55c063831e5e7e8bba62f4a47bd
Jayawardana JTH <49198952+JayawardanaJTH@users.noreply.github.com>
2021-05-23 02:11:17 +0530

    modify login

--- StoreServices/WebContent/login.js
+++ StoreServices/WebContent/login.js
@@ -1,3 +1,49 @@
 /**
- * 
+ * login  functon
  */
+
+$(document).on("click", "#login", function(event) {
+
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+
+	// Form validation-------------------
+	var status = validateItemForm();
+	if (status != true) {
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+
+	var username = $("#uName").vai().trim();
+	var password = $("#password").vai().trim();
+	
+	$.ajax({
+		xhrFields: {
+		        withCredentials: true
+		    },
+	    beforeSend: function (xhr) {
+	        	xhr.setRequestHeader('Authorization', 'Basic ' + btoa(username+":"+password));
+	    	},
+		url: "http://localhost:8080/StoreServices/rest/UserService"
+	}).then(function(data, status, jqxhr) {
+      
+       console.log(data.id);
+    });
+
+});
+
+function validateItemForm() {
+
+	if ($("#uName").val().trim() == "") {
+		return "Enter User Name.";
+	}
+
+	if ($("#password").val().trim() == "") {
+		return "Enter Password.";
+	}
+
+	return true;
+}
