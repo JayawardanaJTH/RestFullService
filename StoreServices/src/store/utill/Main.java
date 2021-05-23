096a99b02d5a93cb34ee6b5cb6887b476a3e91eb
Jayawardana JTH <49198952+JayawardanaJTH@users.noreply.github.com>
2021-05-23 15:59:25 +0530

    add sms gateway

--- StoreServices/src/store/utill/Main.java
+++ StoreServices/src/store/utill/Main.java
@@ -2,11 +2,21 @@ package store.utill;
 
 import java.util.ArrayList;
 
+import com.twilio.Twilio;
+import com.twilio.converter.Promoter; 
+import com.twilio.rest.api.v2010.account.Message; 
+import com.twilio.type.PhoneNumber; 
+ 
+import java.net.URI; 
+import java.math.BigDecimal;
 import store.model.Item;
 import store.model.User;
 
 public class Main {
-
+	// Find your Account Sid and Token at twilio.com/console 
+    public static final String ACCOUNT_SID = "ACd72fb73389057fb4b8934e5c770f9f44"; 
+    public static final String AUTH_TOKEN = "376d8317808f35ca9e4e1b0f8d8df7e6"; 
+    
 	public static void main(String[] args) {
 		IDatabase db = new DatabaseImpl();
 		
@@ -26,6 +36,17 @@ public class Main {
 //		User user = db.getUser("Tiran", "12345");
 		
 //		System.out.println(user.getUserName());
+		
+		Twilio.init(ACCOUNT_SID, AUTH_TOKEN); 
+        Message message = Message.creator( 
+                new com.twilio.type.PhoneNumber("+94769036197"),  
+                "MG87b2ebde5183a5fc59a3f4ab480e2114", 
+                "Your payment is successful ")      
+            .create(); 
+ 
+        System.out.println(message.getSid()); 
+		
+		
 	}
 
 }
